e199c986b55baafa6f5a96c6ff9658ecbd110356
Szymon Gałecki <szymongalecki@gmail.com>
2022-04-25 12:45:04 +0200

    cleaned ii.py

--- ii.py
+++ ii.py
@@ -1,3 +1,5 @@
+"Sequential brute-force solution"
+
 """
 Test 1
 1
@@ -6,13 +8,10 @@ Case #1: 534358
 974.489ms
 """
 
-import time
-
 t = int(input())
 for test_case in range(1, t + 1):
     A, B = [int(_) for _ in input().split()]
     interesting = 0
-    start = time.time()
 
     for num in range(A, B + 1):
         s, p = 0, 1
@@ -23,6 +22,5 @@ for test_case in range(1, t + 1):
             p *= digit
         if p % s == 0:
             interesting += 1
-    end = time.time()
+
     print(f"Case #{test_case}: {interesting}")
-    print(end - start)
